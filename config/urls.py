457d0c51bf9b6255f40e817e2fb156865c15537b
Ehsan Kavian <ehsun.kavian@gmail.com>
2025-10-04 23:24:21 +0330

    message board is redy.

--- config/urls.py
+++ config/urls.py
@@ -25,6 +25,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('message/', include('message.urls'))
+    path('', include('message.urls'))
     # path('', message, name='hello'),
 ]
--- message/views.py
+++ message/views.py
@@ -1,11 +1,12 @@
 # from django.shortcuts import render
-from django.views.generic import TemplateView
-
+from django.views.generic import ListView
+from .models import Message
 # def messageview(request):
 #    return render(request, 'home.html')
 
 
-class MessageView(TemplateView):
+class MessageView(ListView):
+    model = Message
     template_name = 'home.html'
 
 #    def get(self, request):
